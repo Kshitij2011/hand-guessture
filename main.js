1639dc313b163e86438f184242c8a84f312813ca
Kshitij2011 <77619958+Kshitij2011@users.noreply.github.com>
2021-08-05 16:53:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,3 +23,25 @@ classifier = ml5.imageClassifier('');
 
 function modelLoaded(){
     console.log('Model Is Loaded!!!!')}
+
+    function take_snapshot(){
+        Webcam.snap(function(data_uri){
+            document.getElementById("result").innerHTML = '<img id = "captured_image" src = "'+data_uri+'"/>';
+    
+        })
+           
+    }
+    
+    function check(){
+        img = document.getElementById("captured_image");
+        classifier.classify(img, gotResult);
+    }
+    
+    function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+    }
+    }
